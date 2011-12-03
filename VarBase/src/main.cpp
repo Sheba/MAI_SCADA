42f9e2f7ff9bd36d531511ee501a79e214ba1628
Georgy Anosov <cosmovolk@gmail.com>
2011-12-03 19:07:20 +0300

    remake tests

--- VarBase/src/main.cpp
+++ VarBase/src/main.cpp
@@ -1,7 +1,11 @@
 #include "../test/testfile.cpp"
 #include "lib.cpp"
+#include<iostream>
 
 int main()
 {
+    if(!TestAddVar())std::cout<<"add error/n";
+    if(!TestFindVar())std::cout<<"find error/n";
+    if(!TestDeleteVar())std::cout<<"delete error/n";
     return 0;
 }
